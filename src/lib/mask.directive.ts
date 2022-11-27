89bf36a954d1fc6a66400d72be12bfe60122e1e5
lohanna <lohanna.dev@gmail.com>
2022-11-27 11:23:47 -0300

    Improve the code

--- src/lib/mask.directive.ts
+++ src/lib/mask.directive.ts
@@ -25,7 +25,7 @@ export class MaskDirective implements OnInit {
   ngOnInit() {
     this.elementRef.nativeElement.maxLength = this.maskPattern.length;
     this.regexBuilder = new RegexBuilder(this.maskPattern);
-    [this.start, this.groups] = this.regexBuilder.build();
+    this.groups = this.regexBuilder.build();
   }
 
   @HostListener('paste', ['$event'])
@@ -38,32 +38,26 @@ export class MaskDirective implements OnInit {
     if (IGNORE_EVENTS.indexOf(event.inputType) != -1) {
       return
     }
-    
-    console.log(event)
-    this.applyRegex();
+
+    this.checkForMatches();
   }
 
   @HostListener('keydown.Backspace', ['$event']) onDelete() {
-    if (!this.elementRef.nativeElement.value) {
-      this.matches = [];
-      return
-    }
-    let expressions = {...this.start, ...this.groups};
-    this.matches = Object.keys(expressions).filter(
+    this.matches = Object.keys(this.groups).filter(
       rex => new RegExp(rex).exec(this.elementRef.nativeElement.value));
     this.matches.pop();
   }
 
-  checkForMatches(regexs: RegexMap): boolean {
+  checkForMatches(): boolean {
     let oldValue = '';
     let hasMatch = false;
     let replacement = '';
-    for(let rex of Object.keys(regexs)) {
+    for(let rex of Object.keys(this.groups)) {
       if (this.matches.indexOf(rex) != -1) {
         continue
       }
 
-      replacement = regexs[rex];
+      replacement = this.groups[rex];
       oldValue = this.elementRef.nativeElement.value;
       this.elementRef.nativeElement.value = this.elementRef.nativeElement.value.replace(
         new RegExp(rex), replacement);
@@ -75,10 +69,4 @@ export class MaskDirective implements OnInit {
     }
     return hasMatch;
   }
-
-  applyRegex(): void {
-    this.checkForMatches(this.start);
-    this.checkForMatches(this.groups);
-  }
-
 }
--- src/lib/regex-builder.ts
+++ src/lib/regex-builder.ts
@@ -7,30 +7,32 @@ export interface RegexMap {
 export class RegexBuilder {
     constructor(private pattern: string) {}
 
-    build(): RegexMap[] {
+    build(): RegexMap {
         let regex = '';
         let currentGroup = '';
         let regexAcc = `(?<=`;
-        let startRexs: RegexMap = {};
         let charGroups: RegexMap = {};
 
         for (var i = 0; i < this.pattern.length; i++) {
             if (NON_WORD_REGEXP.test(this.pattern[i])) {
-                if (Object.keys(startRexs).length == 0) {
+                if (Object.keys(charGroups).length == 0) {
                     regex =`(?<=^\\d{${currentGroup.length}})`
-                    startRexs[regex] = this.pattern[i];
-                    regexAcc = `^\\d{${currentGroup.length}}\\${this.pattern[i]}`;
-                } else {
-                    regex = `(?<=${regexAcc}\\d{${currentGroup.length}})`;
                     charGroups[regex] = this.pattern[i];
-                    regexAcc += `\\d{${currentGroup.length}}\\${this.pattern[i]}`;
+                    regexAcc = `^\\d{${currentGroup.length}}\\${this.pattern[i]}`;
+                    currentGroup = ''
+                    continue
                 }
+
+                regex = `(?<=${regexAcc}\\d{${currentGroup.length}})`;
+                charGroups[regex] = this.pattern[i];
+                regexAcc += `\\d{${currentGroup.length}}\\${this.pattern[i]}`;
+            
                 currentGroup = ''
                 continue
             }
             currentGroup += this.pattern[i];
         }
-        return [startRexs, charGroups]
+        return charGroups
     }
 
 }
